19bad8a1e85678d25603896db7bc8dc935960ed3
woobaorong <243583785@qq.com>
2025-09-18 22:19:48 +0800

    dsfgsdfg

---
+++ Assets/FootballGameEngine/TestingNewPlayerSetUp/ParentBallToHand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentBallToHand : MonoBehaviour
+{
+    public Transform _ball;
+    public Transform _hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _ball.transform.position = _hand.transform.position;
+        _ball.transform.rotation = _hand.transform.rotation;
+    }
+}
